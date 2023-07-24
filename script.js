e9798ddc5e9eb3a216d4a21e78c6e262f25e0dc2
Karolayne Rodrigues <99221907+Karollayneer@users.noreply.github.com>
2023-07-24 19:20:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Array para armazenar os contadores
 const counters = new Array(26).fill(0); // Contadores para as letras do alfabeto
 
+// Conjunto para armazenar as teclas ativas
+const activeKeys = new Set();
+
 // Função para atualizar o contador
 function updateCounter(index) {
     counters[index]++;
@@ -25,17 +28,31 @@ function displayCounters() {
     }
 }
 
-// Evento de teclado para acionar os contadores
-document.addEventListener('keydown', (event) => {
-    const key = event.key.toUpperCase();
-    if (/^[A-Z]$/.test(key)) {
+// Função para processar as teclas ativas e atualizar os contadores
+function processActiveKeys() {
+    activeKeys.forEach((key) => {
         const index = key.charCodeAt(0) - 65;
         if (index >= 0 && index < counters.length) {
             updateCounter(index);
         }
+    });
+}
+
+// Evento de teclado para adicionar ou remover teclas ativas
+document.addEventListener('keydown', (event) => {
+    const key = event.key.toUpperCase();
+    if (/^[A-Z]$/.test(key)) {
+        activeKeys.add(key);
+        processActiveKeys();
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+    const key = event.key.toUpperCase();
+    if (/^[A-Z]$/.test(key)) {
+        activeKeys.delete(key);
     }
 });
 
 // Inicializar exibição dos contadores
 displayCounters();
-
